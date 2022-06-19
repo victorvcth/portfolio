d905c74fed7b64e804b82ace2b39bff18b07cceb
Victor Hugo Barros <victor_buzetti@hotmail.com>
2022-06-19 20:50:02 -0300

    Atualizando responsividade em algumas telas

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+window.sr = ScrollReveal({ reset: true })
+
+sr.reveal('.sessao-conhecimentos', { duration: 3000 })
+
+sr.reveal('.sessao-projetos', {
+  rotate: { x: 0, y: 80, z: 0 },
+  duration: 2000
+})
+
+sr.reveal('.sessao-contato', { duration: 3000 })
